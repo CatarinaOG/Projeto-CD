0a226ff7c3ab42f28c6605fa133f40e5b61d971b
ASM <a93315@alunos.uminho.pt>
2020-12-04 22:37:36 +0000

    decompressRLE provavelmente certo

--- modulo D/modulo_d.c
+++ modulo D/modulo_d.c
@@ -16,29 +16,25 @@ void removeCharsFromPath (char *path, char *path_new, int n){
 //função que imprime valores de rle repetidos
 void print(FILE *fp, char valor_rle, int n_rep){
     for(int i = 0;i < n_rep; i++)
-        fprintf(fp,"%c",valor_rle); //(ASCII->Valor) 
+        fputc(valor_rle,fp); //(ASCII->Valor) 
 }
 
-//
+//Faz descompressão RLE (Cria um novo ficheiro)
 void decompressRLE(char *path){
     FILE *fp_rle, *fp_new;
-    char ch, str[2], path_new[PATH_MAX_SIZE];
+    char ch, path_new[PATH_MAX_SIZE];
+    int nr_rep;
     removeCharsFromPath(path,path_new,4);
     fp_rle = fopen(path,"r");
     fp_new = fopen(path_new,"a+");    //lê desde o inicio, acrescenta só no fim
     while((ch = fgetc(fp_rle)) != EOF){
         if(ch == '\0'){
-            fgets(str,2,fp_rle);
-            print(fp_new,str[1],(int)str[2]);
+            ch     = fgetc(fp_rle);
+            nr_rep = fgetc(fp_rle);
+            print(fp_new,ch,nr_rep);
         }
         else fputc(ch,fp_new);
     }
     fclose(fp_rle);
     fclose(fp_new);
-}
-
-int main(){
-    char path[] = "aaa.txt.rle";
-    decompressRLE(path);
-    return 0;
 }
