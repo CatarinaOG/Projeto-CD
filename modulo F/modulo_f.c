3839aa24aec1a43ac894b4fc1ae4f26b55c41362
LFFF <a93209@alunos.uminho.pt>
2020-12-05 19:31:26 +0000

    o esqueleto da compressão RLE

--- modulo F/modulo_f.c
+++ modulo F/modulo_f.c
@@ -2,9 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+//64*1024=65536
+void RLEcompression(*fp_origin, *fp_rle){
+	
+}
+
+
 int main(){
-    RLEcompression();
-    freqFile();
-    if(checkCompression()) freqFile();
+    FILE *fp_origin, *fp_RLE, *fp_freq, *fp_RLEfreq;
+
+    RLEcompression(fp_origin, fp_rle);
+
+    freqFile(fp_origin, fp_freq);
+
+    if(checkCompression(fp_freq, fp_RLE)) freqFile(fp_RLE, fp_RLEfreq);
+    else ;//delete RlE File
+
     return 0;
 }
