6314024e72c275090f7621932df0dd0fca518d50
LFFF <a93209@alunos.uminho.pt>
2020-12-06 00:22:06 +0000

    o esqueleto da compressão RLE v2

--- modulo F/modulo_f.c
+++ modulo F/modulo_f.c
@@ -2,21 +2,58 @@
 #include <stdlib.h>
 #include <string.h>
 
+int charLeft(n, *fp_origin){
 
-//64*1024=65536
-void RLEcompression(*fp_origin, *fp_rle){
-	
+}
+
+void firstBlockCompression(f_origin, f_rle){
+	int n =0;
+	while(feof!){
+		
+	}
+}
+
+//64*1024 = 65536
+//65536+1024 = 66560 
+void RLEcompression(*f_origin, *f_rle){
+	int n;
+	int k;
+	int block=1;
+	int rep=0;
+	char repc;
+
+	fopen(f_origin,'r');
+	fopen(f_rle,'a');
+
+	if (firstBlockCompression(f_origin, f_rle)!) break;
+
+	while (feof(f_origin)!){
+
+		if(charLeft(n, fp_origin)>66560){
+			n=0;
+			while(feof(f_origin)! && n<65536) {
+
+				n++;
+			}
+
+		}
+		else{
+
+		}
+	block++;
+	}
+
 }
 
 
 int main(){
-    FILE *fp_origin, *fp_RLE, *fp_freq, *fp_RLEfreq;
+    FILE *f_origin, *f_RLE, *f_freq, *f_RLEfreq;
 
-    RLEcompression(fp_origin, fp_rle);
+    RLEcompression(f_origin, f_rle);
 
-    freqFile(fp_origin, fp_freq);
+    freqFile(f_origin, f_freq);
 
-    if(checkCompression(fp_freq, fp_RLE)) freqFile(fp_RLE, fp_RLEfreq);
+    if(checkCompression(f_freq, f_RLE)) freqFile(f_RLE, f_RLEfreq)
     else ;//delete RlE File
 
     return 0;
