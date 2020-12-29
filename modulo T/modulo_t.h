2f2bb62884ad655d08db4516a1914f8aa547f1b9
a93259 <61979495+a93259@users.noreply.github.com>
2020-12-29 21:58:05 +0000

    Add files via upload

---
+++ modulo T/modulo_t.h
@@ -0,0 +1,15 @@
+#ifndef MODULO_T
+#define MODULO_T
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "modulo_t.h"
+
+int moduloT ( char *sorce_file_Name);
+
+
+#endif
+
+
