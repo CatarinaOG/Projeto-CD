8138dc466506a8dd61d4d621c676fa9541e56081
ASM <a93315@alunos.uminho.pt>
2021-01-02 15:18:42 +0000

    encontraArgumento.c

---
+++ encontraArgumento.c
@@ -0,0 +1,11 @@
+#include "encontraArgumento.h"
+
+/********** Ler input **********/
+
+int encontrouArgumento(char *arg, char argc, char *argv[]){
+    int i = 2;
+    for(;i < argc;i++) 
+        if(!strcmp(arg,argv[i]))
+            return i;
+    return -1;
+}
