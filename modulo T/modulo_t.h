eff603511b09265aa5702ff4a5df78679e861436
a93259 <61979495+a93259@users.noreply.github.com>
2021-01-02 15:50:44 +0000

    Update modulo_t.h

--- modulo T/modulo_t.h
+++ modulo T/modulo_t.h
@@ -1,15 +1,9 @@
 #ifndef MODULO_T
 #define MODULO_T
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include "modulo_t.h"
-
 int moduloT ( char *file);
 
-
 #endif
+
 
 
