8e7b2b41e4f12a3ccadff1eee5bd4080a3d91ead
DivineWingedDemon <58912131+DivineWingedDemon@users.noreply.github.com>
2021-01-03 21:54:31 +0000

    Update modulo_f.h

--- modulo F/modulo_f.h
+++ modulo F/modulo_f.h
@@ -23,9 +23,19 @@ typedef struct freqFileBuild_output {
 
 
 // Esta funcao vai retornar 0 se correr ate ao fim sem erros.
+// A funcao cria e escreve o ficheiro RLE, se for forçado ou se a compressão do primeiro bloco não for menor que 5%. e cria a lista de struct freqList, para otimizar .
+// 
+int applyRLECompression (FILE *fp_origin, BFreq *freqList, char *fileName, int checkCom, int blockSizeMultiple, float *compression);
+
+// Esta funcao vai retornar 0 se correr ate ao fim sem erros e -2.
+// A funcao cria e escreve os ficheiros freq e RLE.freq.
+// Para isso, ela vai usar a lista de struct freqList e o número de blocks, criada e obtido na função "applyRLECompression".
+FFBout freqFileBuild (BFreq freqList, int nblock, char *fileName);
+
+// Esta funcao vai retornar 0 se correr ate ao fim sem erros.
 // A funcao vai fazer a compressao RLE, escrever no ficheiro as frequencias.
 // A funcao cria um ficheiro com as frequencias dos caracteres no ficheiro original quer seja realizada compressao ou nao.
-// Caso seja realizaca compressao RLE entao tambem cria um ficheiro com o conteudo apos sofrer as alteracoes necessarias
+// Caso seja realizada compressao RLE entao tambem cria um ficheiro com o conteudo apos sofrer as alteracoes necessarias
 //   para a compressao, e um ficheiro com as frequencias dos caracteres do mesmo.
 int moduloF (int argc, char **argv);
 
