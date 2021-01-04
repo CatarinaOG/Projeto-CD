6cf66eb35d41af0dad21ee4c585195b01cceecaf
LFFF <a93209@alunos.uminho.pt>
2021-01-04 02:15:44 +0000

    OK

--- modulo F/shafa.c
+++ modulo F/shafa.c
@@ -1,5 +1,12 @@
-#include "../encontraArgumento.h"
 #include "modulo_f.h"
+
+int encontrouArgumento(char *arg, int argc, char *argv[]){
+    int i = 2;
+    for(;i < argc;i++) 
+        if(!strcmp(arg,argv[i]))
+            return i;
+    return -1;
+}
 
 int main(int argc, char *argv[]) {
     if(argc > 1){   
